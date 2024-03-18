a845aa17d083527812a1f5082fd1f510cc7e4d46
Yurii Ihnatchuk <yihnatchuk@c14236.netis.cc>
2024-03-18 02:45:46 +0200

    add suspense

--- app/(auth)/signIn/page.jsx
+++ app/(auth)/signIn/page.jsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import { Form } from '@/components/Form/Form';
 
 export default function SignInPage() {
-    return <Form />;
+    return (
+        <Suspense>
+            <Form />
+        </Suspense>
+    );
 }
