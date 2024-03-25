af97ff6cb7940014ca7b2d85134ea9e156f12882
uuuriiy <ihnatchukud@gmail.com>
2024-03-25 18:11:29 +0200

    test v36

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -38,6 +38,7 @@ export async function POST(req) {
             { status: 201 },
         );
     } catch (error) {
+        console.error(error);
         return NextResponse.json({ message: error.message }, { status: 500 });
     }
 }
