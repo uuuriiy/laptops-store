38b6d277f4345c74a9c6834353b5a0bf9b411291
uuuriiy <ihnatchukud@gmail.com>
2024-03-26 16:48:06 +0200

    test v40

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import { hash } from 'bcrypt';
+import { cookies } from 'next/headers';
 import {
     sendVerificationEmail,
     generateEmailVerificationToken,
@@ -7,6 +8,8 @@ import {
 import { checkUserExistance, createUser } from '@/lib/query';
 
 export async function POST(req) {
+    cookies().set('url', req.url);
+    console.log(req.url, 'req');
     try {
         const body = await req.json();
         const { email, username, password } = body;
