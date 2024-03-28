a38a89aaea752c459c0f09b6b11347320e727aa4
uuuriiy <ihnatchukud@gmail.com>
2024-03-28 19:38:36 +0200

    test 52

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -8,6 +8,7 @@ import {
 import { checkUserExistance, createUser } from '@/lib/query';
 
 export async function POST(req) {
+    console.log(req.method, 'method');
     try {
         const body = await JSON.parse(req);
         // req.json();
